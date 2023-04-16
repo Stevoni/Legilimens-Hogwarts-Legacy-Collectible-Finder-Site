57424a2418f0cbe4e5c6b9fceae1bf9773b75648
Stevoni <tanner65@gmail.com>
2023-04-16 16:45:21 -0500

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Legilimens header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Legilimens Collectible Finder/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders file upload button', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Select file to upload/i);
   expect(linkElement).toBeInTheDocument();
 });
