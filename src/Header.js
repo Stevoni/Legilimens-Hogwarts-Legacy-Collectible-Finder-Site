1c35ad546a61d85085dbd2df5e2a91ad637c128e
Stevoni <tanner65@gmail.com>
2023-04-16 14:01:11 -0500

    Updated header and added collectibles file

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 
 function Header() {
-    const repoAddress = "https://github.com/Malin001/Legilimens-Hogwarts-Legacy-Collectible-Finder"
+    const repoAddress = "https://github.com/Stevoni/Legilimens-Hogwarts-Legacy-Collectible-Finder-Site"
+    const originalRepoAddress = "https://github.com/Malin001/Legilimens-Hogwarts-Legacy-Collectible-Finder"
 
     return (
         <header>
             <h1>Legilimens Collectible Finder</h1>
             <h3>A Hogwarts Legacy tool to find your missing collectibles <br/>
-               <a href={repoAddress}>{repoAddress}</a></h3>
+               <a href={repoAddress}>{repoAddress}</a>.
+            </h3>
+            <h3>
+                Based on the python tool originally created by Malin001 <br/>
+                <a href={originalRepoAddress}>{originalRepoAddress}</a>.
+            </h3>
         </header>
     );
 }
